3048cccc9c080bcef8810915cb8b22d40081384f
MAHALAKSHMImahalakshmi <athamahalakshmi27@gmail.com>
2025-09-05 18:07:43 +0530

    bastion

--- 20-bastin/main.tf
+++ 20-bastin/main.tf
@@ -7,9 +7,16 @@ resource "aws_instance" "bastin" {
   
   instance_type = "t3.micro"
 // create security group for instances
-vpc_security_group_ids = [local.bastin_sg_id] // in list 
-subnet_id =  local.public_subnet_id  
+  vpc_security_group_ids = [local.bastin_sg_id] // in list 
+  subnet_id =  local.public_subnet_id  
 // what about subnet ?
+
+ # need more space for terraform
+  root_block_device {
+    volume_size = 50
+    volume_type = "gp3" # or "gp2", depending on your preference
+  }
+  user_data = file("bastion.sh")
    tags = merge(
     local.common_tags,
     {
